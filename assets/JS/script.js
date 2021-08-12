252d692d9fac172f0d76b2363b96c398f9843274
Kevin Kim <kevin.shihyun@gmail.com>
2021-08-12 14:09:42 -0600

    Bootstrap colors

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,20 +1,3 @@
-// var dateRange = "2021-08-11";
-
-const settings = {
-  async: true,
-  crossDomain: true,
-  url: "https://billboard-api2.p.rapidapi.com/hot-100?date=2021-08-11&range=1-100",
-  method: "GET",
-  headers: {
-    "x-rapidapi-key": "7da31efff8mshbe2b34b9f658331p18aaf2jsn2df62bd40cf9",
-    "x-rapidapi-host": "billboard-api2.p.rapidapi.com",
-  },
-};
-
-$.ajax(settings).done(function (response) {
-  console.log(response);
-});
-
 // HTML Script ****************************************************************************
 window.addEventListener("DOMContentLoaded", (event) => {
   // Navbar shrink function
